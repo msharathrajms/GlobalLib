bcd1bb7c23446c6b46a5bf0567b82be8aaded1f7
msharathraj <40004550+msharathraj@users.noreply.github.com>
2019-10-19 10:01:19 +0530

    Update Sample.groovy

--- Sample.groovy
+++ Sample.groovy
@@ -1,11 +1,5 @@
 job('Test-Artifactory'){
 	
-	def server = Artifactory.server 'jenkins-artifactory-server' , username: 'admin', password: 'password'
-	def rtMaven = Artifactory.newMavenBuild()
-	rtMaven.resolver server: server, releaseRepo: 'sample-repo', snapshotRepo: 'sample-repo-snapshot'
-	rtMaven.deployer server: server, releaseRepo: 'sample-repo-local', snapshotRepo: 'sample-repo-local'
-	rtMaven.deployer.deployArtifacts = true
-	
 		parameters {
 			choiceParam('SOURCE_BRANCH', ['Master', 'Develop'], 'Source branch from code is merged to Destination')
 			choiceParam('DESTINATION_BRANCH', ['Master', 'Release'], 'Destination branch where the code should be merged')
